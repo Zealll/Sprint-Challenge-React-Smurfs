10e3a4741c661abb9c2bffc8e87d7886601c8836
unknown <eriznis@gmail.com>
2019-03-01 11:20:43 -0500

    Nav bar created, and data fetched from Axios

--- village/src/App.js
+++ village/src/App.js
@@ -1,16 +1,25 @@
 import React, { Component } from 'react';
-import { Route } from 'react-router-dom'
+import { Route, NavLink } from 'react-router-dom'
 
 import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
+import Axios from 'axios';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
       smurfs: [],
+      error:'',
     };
+  }
+
+  componentDidMount(){
+    Axios
+    .get('http://localhost:3333/smurfs')
+    .then(res => {this.setState({smurfs: res.data})})
+    .catch(err => {this.setState({error: err})})
   }
   // add any needed code to ensure that the smurfs collection exists on state and it has data coming from the server
   // Notice what your map function is looping over and returning inside of Smurfs.
@@ -18,6 +27,14 @@ class App extends Component {
   render() {
     return (
       <div className="App">
+        <div className='top'>
+        <h1>Welcome to Number One Smurf Page on the Web</h1>
+          <nav>
+            <NavLink className='a' to='/smurf-form'>Add a Smurf</NavLink>
+            <NavLink className='a' to='/'>Home</NavLink>
+
+          </nav>
+        </div>
         <Route path='/smurf-form' render={() => <SmurfForm />}/>
         <Route exact path='/' render={props => <Smurfs {...props} smurfs={this.state.smurfs} />} />
       </div>
