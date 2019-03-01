4cad11ef384bcf8794a5d677efa57870bb633144
unknown <eriznis@gmail.com>
2019-03-01 11:58:51 -0500

    MVP finished, started working on the router Stretch

--- village/src/App.js
+++ village/src/App.js
@@ -4,6 +4,7 @@ import { Route, NavLink } from 'react-router-dom'
 import './App.css';
 import SmurfForm from './components/SmurfForm';
 import Smurfs from './components/Smurfs';
+import Smurf from './components/Smurf'
 import Axios from 'axios';
 
 class App extends Component {
@@ -45,6 +46,7 @@ class App extends Component {
         </div>
         <Route path='/smurf-form' render={props => <SmurfForm {...props} addSmurf={this.addSmurf}/>}/>
         <Route exact path='/' render={props => <Smurfs {...props} smurfs={this.state.smurfs} />} />
+        <Route exact path='/smurf/:apple' render={props => <Smurf {...props} smurfs={this.state.smurfs} />} />
       </div>
     );
   }
--- village/src/components/Smurf.js
+++ village/src/components/Smurf.js
@@ -1,12 +1,15 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom'
 
 const Smurf = props => {
   return (
+    <NavLink className='smurf' to={`/smurf/${props.smurf.id}`}>
     <div className="Smurf">
       <h3>{props.name}</h3>
       <strong>{props.height} tall</strong>
       <p>{props.age} smurf years old</p>
     </div>
+    </NavLink>
   );
 };
 
